6ecb8e0d4ffe04e3da6cafed092547ef57fe3024
Jerry Jeon <jerry.jeon@sendbird.com>
2024-04-07 18:24:43 +0900

    Implemented after codes

--- integration/after/src/App.tsx
+++ integration/after/src/App.tsx
@@ -1,15 +1,96 @@
-import React, {useState} from 'react';
+import {PropsWithChildren, useState} from 'react';
+
+import {useSendbirdStateContext} from '@sendbird/uikit-react';
+import {GroupChannel} from '@sendbird/uikit-react/GroupChannel';
+import SendbirdProvider from '@sendbird/uikit-react/SendbirdProvider';
+import '@sendbird/uikit-react/dist/index.css';
 import './App.css';
 
 const orderDetails = {
-    orderNumber: 'Food Delivery Order #12345',
+    orderNumber: 'Order #12345',
     items: ['Pizza', 'Coke'],
     status: 'In transit',
-    deliveryPersonId: 'delivery',
+    deliveryPersonId: 'YOUR_DELIVERY_PERSON_ID', // TODO Replace 'YOUR_DELIVERY_PERSON_ID' with the actual delivery person's user ID
+};
+
+const App = () => {
+    const [userId, setUserId] = useState('');
+    const [showLogin, setShowLogin] = useState(true);
+    const [showChat, setShowChat] = useState(false); // State to toggle the visibility of the chat
+    const [channelUrl, setChannelUrl] = useState(''); // State to store the channel URL
+
+    const handleLogin = (e: any) => {
+        e.preventDefault();
+        if (userId.trim()) setShowLogin(false);
+    };
+
+    const handleCreateChannel = (url: string) => {
+        setChannelUrl(url);
+        setShowChat(true);
+    };
+
+    const renderLoginForm = () => {
+        return (
+            <form className={'login-form'} onSubmit={handleLogin}>
+                <input type="text" placeholder="Enter your user ID" value={userId} onChange={(e) => setUserId(e.target.value)} />
+                <button type="submit">Login</button>
+            </form>
+        );
+    };
+
+    const renderApp = () => {
+        const APP_ID = 'YOUR_APP_ID'; // TODO Reapcle 'YOUR_APP_ID' with the actual Sendbird application ID
+        return (
+            <div className="app-container">
+                <SendbirdProvider appId={APP_ID} userId={userId}>
+                    <OrderStatus orderDetails={orderDetails}>
+                        {showChat ? (
+                            <button onClick={() => setShowChat(false)}>{'Close'}</button>
+                        ) : (
+                            <ChatButton userId={userId} deliveryPersonId={orderDetails.deliveryPersonId} onCreateChannel={handleCreateChannel} />
+                        )}
+                    </OrderStatus>
+                    {showChat && <GroupChannel channelUrl={channelUrl} />}
+                </SendbirdProvider>
+            </div>
+        );
+    };
+
+    return <div className="App">{showLogin ? renderLoginForm() : renderApp()}</div>;
+};
+
+const useCreateChannel = (userId: string, deliveryPersonId: string, onCreateChannel: (url: string) => void) => {
+    // To use the context, the component should be wrapped in the SendbirdProvider.
+    const { stores } = useSendbirdStateContext(); // Access the Sendbird state context
+    const sdk = stores.sdkStore.sdk; // Get the Sendbird SDK instance
+
+    return async () => {
+        // Ensure that SDK is initialized
+        if (!sdk || !sdk.groupChannel) {
+            console.log('SDK not initialized');
+            return;
+        }
+
+        // Use the SDK to create a new group channel
+        const params = {
+            invitedUserIds: [userId, deliveryPersonId],
+            isDistinct: true, // Reuse the channel if it already exists
+        };
+
+        // In production, you should handle the error using try-catch
+        const channel = await sdk.groupChannel.createChannel(params);
+        onCreateChannel(channel.url);
+    };
+};
+
+type ChatButtonProps = { userId: string; deliveryPersonId: string; onCreateChannel: (url: string) => void };
+const ChatButton = ({ userId, deliveryPersonId, onCreateChannel }: ChatButtonProps) => {
+    const onStartChat = useCreateChannel(userId, deliveryPersonId, onCreateChannel);
+    return <button onClick={onStartChat}>{'Chat with Delivery Person'}</button>;
 };
 
 type OrderStatusProps = { orderDetails: typeof orderDetails };
-const OrderStatus = ({orderDetails, children}: React.PropsWithChildren<OrderStatusProps>) => {
+const OrderStatus = ({ orderDetails, children }: PropsWithChildren<OrderStatusProps>) => {
     return (
         <div className="order-container">
             <div className="order-body">
@@ -27,36 +108,6 @@ const OrderStatus = ({orderDetails, children}: React.PropsWithChildren<OrderStat
             </div>
         </div>
     );
-};
-
-const App = () => {
-    const [userId, setUserId] = useState('');
-    const [showLogin, setShowLogin] = useState(true);
-
-    const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (userId.trim()) setShowLogin(false);
-    };
-
-    const renderLoginForm = () => {
-        return (
-            <form className={'login-form'} onSubmit={handleLogin}>
-                <input type="text" placeholder="Enter your user ID" value={userId}
-                       onChange={(e) => setUserId(e.target.value)}/>
-                <button type="submit">Login</button>
-            </form>
-        );
-    };
-
-    const renderApp = () => {
-        return (
-            <div className="app-container">
-                <OrderStatus orderDetails={orderDetails}></OrderStatus>
-            </div>
-        );
-    };
-
-    return <div className="App">{showLogin ? renderLoginForm() : renderApp()}</div>;
 };
 
 export default App;
